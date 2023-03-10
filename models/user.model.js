eef5a0e7e23243668981c3cd022d886729d51443
Eliska Ryklova <ryklovae@coventry.ac.uk>
2023-03-10 12:03:10 +0000

    User routes added - view all users - view user details by id - delete user - add user

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,8 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const sequelize = require('../db');
 const User = sequelize.define("users", {
   ID: {
@@ -33,4 +36,52 @@ sequelize.sync({ alter: true }).then(() => {
    console.error('Unable to create table User: ', error);
 });
 
-module.exports = User;
+// get user by email
+exports.getByEmail = async function getByEmail(email) {
+  let result = await User.findAll({
+    where: {
+      email: email
+    }
+  });
+  return result;
+}
+
+// get user by id
+exports.getById = async function getById(id) {
+  let result = await User.findAll({
+    where: {
+      ID: id
+    }
+  });
+  return result;
+}
+
+// get all users
+exports.getAll = async function getAll() {
+  let result = await User.findAll({
+    attributes: ['ID', 'name', 'email', 'role']  // exclude password
+  });
+  return result;
+}
+
+// create user
+exports.createUser = async function createUser(user) {
+  // create password hash
+  const hash = bcrypt.hashSync(user.password, saltRounds);
+  user.password = hash;
+  
+  let result = await User.create(user);
+  return result;
+}
+
+// delete user
+exports.deleteUser = async function deleteUser(id) {
+  let result = await User.destroy({
+    where: {
+      ID: id
+    }
+  });
+  return result;
+}
+
+exports.User = User;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,36 @@
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
-const router = Router({prefix: '/api/v1/users'})
+const model = require('../models/user.model.js');
 
-router.get('/', getAll)
+const router = Router({prefix: '/api/v1/users'});
+
+router.get('/', getAll);
+router.get('/:id([0-9]{1,})', getById);
+router.del('/:id([0-9]{1,})', deleteUser);
+router.post('/', bodyParser(), createUser);
 
 async function getAll(ctx) {
-  let users = ""
-  ctx.body = users
+  let users = await model.getAll();
+  ctx.body = users;
+}
+
+async function getById(ctx) {
+  let id = ctx.params.id;
+  let user = await model.getById(id);
+  ctx.body = user;
+}
+
+async function createUser(ctx) {
+  let user = ctx.request.body;
+  let users = await model.createUser(user);
+  ctx.body = users;
+}
+
+async function deleteUser(ctx) {
+  let id = ctx.params.id;
+  let rowsAffected = await model.deleteUser(id);
+  ctx.body = rowsAffected;
 }
 
 module.exports = router;
